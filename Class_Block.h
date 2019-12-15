a12a12aaeef5583b13fba65a5677a7c30e5621c1
Klein4C <lczwz1606@gmail.com>
2019-12-15 09:49:25 -0600

    stable version of phase-1
    >DFS/topologic used
    >coordinate generating normally
    >instant output n10-300.fp

--- Class_Block.h
+++ Class_Block.h
@@ -20,15 +20,18 @@ class Block
         vector<Block*> right;
         vector<Block*> up;
         vector<Block*> down;
-        // Block *pathFromVer;
-        // Block *pathFromHor;
-        // Block *pathToVer;
-        // Block *pathToHor;
         void direction(bool beforeP, bool beforeN, Block *targetBlk);
-        void pushUp();
-        void pushDn();
-        void pushLf();
-        void pushRt();
+        unsigned int pNum;
+        unsigned int nNum;
+        // ----for topological/DFS
+        int DFSstateUp = -1; // -1:not visited  0:visiting 1:visited
+        Block* DFSpredUp = NULL;
+        int dUp = -1; // discovery time
+        int fUp = -1; // finish time
+        int DFSstateRt = -1; // -1:not visited  0:visiting 1:visited
+        Block* DFSpredRt = NULL;
+        int dRt = -1; // discovery time
+        int fRt = -1; // finish time
     private:
         unsigned int blk_num;
         unsigned int blk_height=0;
@@ -109,13 +112,11 @@ void Block::direction(bool beforeP, bool beforeN, Block *targetBlk)
         {
             left.push_back(targetBlk);
             // cout<<blk_num<<" left push: "<<targetBlk->blk_num<<endl;
-            // pushLf();
         }
         else
         {
             up.push_back(targetBlk);
             // cout<<blk_num<<" up push: "<<targetBlk->blk_num<<endl;
-            // pushUp();
         }
     }
     else
@@ -124,92 +125,14 @@ void Block::direction(bool beforeP, bool beforeN, Block *targetBlk)
         {
             down.push_back(targetBlk);
             // cout<<blk_num<<" down push: "<<targetBlk->blk_num<<endl;
-            // pushDn();
         }
         else
         {
             right.push_back(targetBlk);
             // cout<<blk_num<<" right push: "<<targetBlk->blk_num<<endl;
-            // pushRt();
         }
     }
     
-}
-
-void Block::pushUp()
-{
-    if (up.size()>0)
-    {
-        for (int i=0; i<up.size(); i++)
-        {
-            if (bottomLeft_y+blk_height > up[i]->getLLy())
-            {
-                // cout<<blk_num<<" pushes "<<up[i]->blk_num<<" up to "<<bottomLeft_y+blk_height<<endl;
-                up[i]->setLL(up[i]->getLLx(), bottomLeft_y+blk_height);
-            }
-        }
-        for (int i=0; i<up.size(); i++)
-        {
-            up[i]->pushUp();
-        }
-    }
-}
-
-void Block::pushRt()
-{
-    if (right.size()>0)
-    {
-        for (int i=0; i<right.size(); i++)
-        {
-            if (bottomLeft_x+blk_width > right[i]->getLLx())
-            {
-                // cout<<blk_num<<" pushes "<<right[i]->blk_num<<" right to "<<bottomLeft_x+blk_width<<endl;
-                right[i]->setLL(bottomLeft_x+blk_width, right[i]->getLLy());
-            }
-        }
-        for (int i=0; i<right.size(); i++)
-        {
-            right[i]->pushRt();
-        }
-    }
-}
-
-void Block::pushDn()
-{
-    if (down.size()>0)
-    {
-        for (int i=0; i<down.size(); i++)
-        {
-            if (down[i]->getLLy()+down[i]->getHeight() > bottomLeft_y)
-            {
-                // cout<<blk_num<<" pushes "<<down[i]->blk_num<<" down to "<<(bottomLeft_y-down[i]->getHeight())<<endl;
-                down[i]->setLL(down[i]->getLLx(), bottomLeft_y-down[i]->getHeight());
-            }
-        }
-        for (int i=0; i<down.size(); i++)
-        {
-            down[i]->pushDn();
-        }
-    }
-}
-
-void Block::pushLf()
-{
-    if (left.size()>0)
-    {
-        for (int i=0; i<left.size(); i++)
-        {
-            if (left[i]->getLLx()+left[i]->getWidth() > bottomLeft_x)
-            {
-                // cout<<blk_num<<" pushes "<<left[i]->blk_num<<" left to "<<(bottomLeft_x-left[i]->getWidth())<<endl;
-                left[i]->setLL(bottomLeft_x-left[i]->getWidth(), left[i]->getLLy());
-            }
-        }
-        for (int i=0; i<left.size(); i++)
-        {
-            left[i]->pushLf();
-        }
-    }
 }
 
 
--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ using namespace std;
 
 int readFile(char* fName, vector<Block> &blkLib, vector<Net> &netLib, int &blk_amt);
 int randGen(vector<Block> &blkLib, int &blk_amt, vector<unsigned int> &seqP, vector<unsigned int> &seqN, int &max_w, int &max_h);
+void DFSvisit(vector<Block> &blkLib, Block &targetBlk, int &timeStamp, vector<Block*> &Q, bool directUp); // directUp= 1:up  0:right
 
 int main(int argc, char* argv[])
 {
@@ -27,10 +28,10 @@ int main(int argc, char* argv[])
     vector<Block> blkLib;
     vector<Net> netLib;
     // NOTE: please use the non-initialized vectors instead to implmt randomness
-    // vector<unsigned int> seqP;
-    // vector<unsigned int> seqN;
-    vector<unsigned int> seqP{4,2,1,8,5,9,0,6,3,7};
-    vector<unsigned int> seqN{9,7,6,3,1,4,2,5,8,0};
+    vector<unsigned int> seqP;
+    vector<unsigned int> seqN;
+    // vector<unsigned int> seqP{4,2,1,8,5,9,0,6,3,7};
+    // vector<unsigned int> seqN{9,7,6,3,1,4,2,5,8,0};
     int max_h=0;
     int max_w=0;
     int blk_n = 0;
@@ -41,41 +42,58 @@ int main(int argc, char* argv[])
     randGen(blkLib, blk_n, seqP, seqN, max_w, max_h);
 
     // ========call out all blocks========
-
-    // call out sequence values
+    string outputName = (string)argv[1]+"_Li_Chengzhe.out1";
+    cout<<">>>Opening output file: "<<outputName<<endl;
+    ofstream outFile(outputName);
+    // --------call out sequence values--------
     cout<<"P seq: ";
+    outFile<<"P seq: ";
     for (int i=0; i<blk_n; i++)
     {
         cout<<seqP[i]<<" ";
+        outFile<<seqP[i]<<" ";
     }
     cout<<"\nN seq: ";
+    outFile<<"\nN seq: ";
     for (int i=0; i<blk_n; i++)
     {
         cout<<seqN[i]<<" ";
+        outFile<<seqN[i]<<" ";
     }
     cout<<endl;
-    // call out dimensions and area
+    outFile<<endl;
+    // --------call out dimensions and area--------
     cout<<"Chip Dimension(WxH): "<<max_w<<" "<<max_h<<endl;
+    outFile<<"Chip Dimension(WxH): "<<max_w<<" "<<max_h<<endl;
     long int A = max_w*max_h;
     cout<<"Chip Area: "<<A<<endl;
-    // call out blocks with coordinations
+    outFile<<"Chip Area: "<<A<<endl;
+    // --------call out blocks with coordinations--------
     cout<<"------Blocks------"<<endl;
+    outFile<<"------Blocks------"<<endl;
     for (vector<Block>::iterator itr=blkLib.begin(); itr!=blkLib.end(); itr++)
     {
-        cout<<"NO."<<itr->getNum()<<" "<<itr->getWidth()<<" "<<itr->getHeight()<<" ";
-        cout<<"x@"<<itr->getLLx()<<" y@"<<itr->getLLy()<<endl;
+        cout<<"NO."<<itr->getNum()<<" "<<itr->getWidth()<<" "<<itr->getHeight()\
+        <<" x@"<<itr->getLLx()<<" y@"<<itr->getLLy()<<endl;
+        outFile<<"NO."<<itr->getNum()<<" "<<itr->getWidth()<<" "<<itr->getHeight()\
+        <<" x@"<<itr->getLLx()<<" y@"<<itr->getLLy()<<endl;
     }
-    // call out nets
-    cout<<"\n------Nets------"<<endl;
-    for (vector<Net>::iterator itr=netLib.begin(); itr!=netLib.end(); itr++)
-    {
-        cout<<itr->getNum()<<" "<<itr->getDeg();
-        for (unsigned int k=0; k<itr->getDeg(); k++)
-        {
-            cout<<" "<<itr->connBlks[k];
-        }
-        cout<<endl;
-    }
+    // --------call out nets--------
+    // cout<<"\n------Nets------"<<endl;
+    // outFile<<"\n------Nets------"<<endl;
+    // for (vector<Net>::iterator itr=netLib.begin(); itr!=netLib.end(); itr++)
+    // {
+    //     cout<<"NO."<<itr->getNum()<<" "<<itr->getDeg();
+    //     outFile<<"NO."<<itr->getNum()<<" "<<itr->getDeg();
+    //     for (unsigned int k=0; k<itr->getDeg(); k++)
+    //     {
+    //         cout<<" "<<itr->connBlks[k];
+    //         outFile<<" "<<itr->connBlks[k];
+    //     }
+    //     cout<<endl;
+    //     outFile<<endl;
+    // }
+    // outFile.close();
 
     return 0;
 }
@@ -197,73 +215,52 @@ int randGen(vector<Block> &blkLib, int &blk_amt, vector<unsigned int> &seqP, vec
 
     // ========random sequencing========
     // NOTE: please uncomment the following code block to really implementing randomness
-    // unsigned int seed = chrono::system_clock::now().time_since_epoch().count();
-    // shuffle(seqP.begin(), seqP.end(), default_random_engine(seed));
-    // seed = chrono::system_clock::now().time_since_epoch().count();
-    // shuffle(seqN.begin(), seqN.end(), default_random_engine(seed));
+    unsigned int seed = chrono::system_clock::now().time_since_epoch().count();
+    shuffle(seqP.begin(), seqP.end(), default_random_engine(seed));
+    seed = chrono::system_clock::now().time_since_epoch().count();
+    shuffle(seqN.begin(), seqN.end(), default_random_engine(seed));
 
 
 
     // ========storing relative position========
-    bool beforeP = true;
-    bool beforeN = true;
-    for (int blk_idx=0; blk_idx<blk_amt; blk_idx++)
+    cout<<">>>storing relative position"<<endl;
+    for (int idx=0; idx<blk_amt; idx++) // O(n)
     {
-        beforeP = true;
-        // cout<<"["<<blk_idx<<"]:----------------"<<endl;
-        for (int idxp=0; idxp<blk_amt; idxp++)
+        blkLib[seqP[idx]].pNum = idx;
+        blkLib[seqN[idx]].nNum = idx;
+    }
+    for (int i=0; i<blk_amt; i++) // O(n^2)
+    {
+        bool beforeP = true;
+        bool beforeN = true;
+        // cout<<"For BLK["<<i<<"]("<<blkLib[i].pNum<<","<<blkLib[i].nNum<<"):"<<endl;
+        for (int j=0; j<blk_amt; j++)
         {
-            if (seqP[idxp]==blk_idx)
+            if (i!=j)
             {
-                // cout<<"<<"<<seqP[idxp]<<"="<<blk_idx<<endl;
-                beforeP = false;
-                continue;
-            }
-            beforeN = true;
-            for (int idxn=0; idxn<blk_amt; idxn++)
-            {
-                if (seqN[idxn]==blk_idx)
-                {
-                    beforeN = false;
-                    continue;
-                }
-                if (seqP[idxp]==seqN[idxn])
-                {
-                    blkLib[blk_idx].direction(beforeP, beforeN, &blkLib[seqP[idxp]]);
-                    break;
-                }
+                beforeP = (blkLib[j].pNum<blkLib[i].pNum);
+                beforeN = (blkLib[j].nNum<blkLib[i].nNum);
+                // cout<<"        ["<<j<<"]("<<blkLib[j].pNum<<","<<blkLib[j].nNum<<"): "<<beforeP<<"-"<<beforeN<<"  ";
+                blkLib[i].direction(beforeP, beforeN, &blkLib[j]);
             }
         }
     }
 
-    // for (int k=0; k<blk_amt; k++)
-    // {
-    //     cout<<"BLK["<<k<<"]:"<<endl;
-    //     cout<<"up: ";
-    //     for (int i=0; i<blkLib[k].up.size(); i++)
-    //     {
-    //         cout<<blkLib[k].up[i]->getNum()<<" ";
-    //     }
-    //     cout<<"\ndown: ";
-    //     for (int i=0; i<blkLib[k].down.size(); i++)
-    //     {
-    //         cout<<blkLib[k].down[i]->getNum()<<" ";
-    //     }
-    //     cout<<"\nleft: ";
-    //     for (int i=0; i<blkLib[k].left.size(); i++)
-    //     {
-    //         cout<<blkLib[k].left[i]->getNum()<<" ";
-    //     }
-    //     cout<<"\nright: ";
-    //     for (int i=0; i<blkLib[k].right.size(); i++)
-    //     {
-    //         cout<<blkLib[k].right[i]->getNum()<<" ";
-    //     }
-    //     cout<<"\n"<<endl;
-    // }
 
     // ========finding critical path========
-    Block dummyBottom;
+    cout<<">>>finding critical path"<<endl;
+    vector<Block*> topoQUp;
+    vector<Block*> topoQRt;
+    int timeStamp;
+    for  (vector<Block>::iterator itr=blkLib.begin(); itr!=blkLib.end(); itr++)
+    {
+        itr->DFSstateUp = -1;
+        itr->DFSpredUp = NULL;
+        itr->DFSstateRt = -1;
+        itr->DFSpredRt = NULL;
+    }
+    // cout<<">>>creating dummies"<<endl;
+    Block dummyBottom; // dummies have default w and h as 0
     Block dummyTop;
     Block dummyLeft;
     Block dummyRight;
@@ -286,28 +283,118 @@ int randGen(vector<Block> &blkLib, int &blk_amt, vector<unsigned int> &seqP, vec
             dummyRight.left.push_back(&blkLib[i]);
         }
     }
-    // cout<<"____PUSH UP____"<<endl;
-    dummyBottom.pushUp();
-    // cout<<"____PUSH DN____"<<endl;
-    // dummyTop.pushDn();
-    // cout<<"____PUSH RT____"<<endl;
-    dummyLeft.pushRt();
-    // cout<<"____PUSH LF____"<<endl;
-    // dummyRight.pushLf();
 
-    // ========extract max dimensions========
-    // int x_min=0, x_max=0, y_min=0, y_max=0;
+    dummyBottom.DFSstateUp = -1;
+    dummyBottom.DFSpredUp = NULL;
+    dummyBottom.dUp = -1;
+    dummyBottom.fUp = -1;
+    dummyBottom.setWidth(0);
+    dummyBottom.setHeight(0);
+    dummyBottom.setLL(0, 0);
+    dummyLeft.DFSstateRt = -1;
+    dummyLeft.DFSpredRt = NULL;
+    dummyLeft.dRt = -1;
+    dummyLeft.fRt = -1;
+    dummyLeft.setWidth(0);
+    dummyLeft.setHeight(0);
+    dummyLeft.setLL(0, 0);
+
+    // cout<<">>>starting DFS"<<endl;
+    timeStamp = 0;
+    DFSvisit(blkLib, dummyBottom, timeStamp, topoQUp, true);
+
+    timeStamp = 0;
+    DFSvisit(blkLib, dummyLeft, timeStamp, topoQRt, false); 
+
+    for (int i=1; i<topoQUp.size(); i++) // [0] will be dummy, assume least 1 cell
+    {
+        // topoQUp.size() equal to topoQRt.size() = #of cells = blk_amt
+        for (int j=0; j<topoQRt[i]->left.size(); j++)
+        {
+            int newX = topoQRt[i]->left[j]->getLLx()\
+                        +topoQRt[i]->left[j]->getWidth();
+            if (newX>topoQRt[i]->getLLx())
+            {
+                topoQRt[i]->setLL(newX, topoQRt[i]->getLLy());
+                // cout<<"  x refreshed to: "<<newX<<endl;
+            }
+            else
+            {
+                // cout<<"  x remained tobe: "<<topoQRt[i]->getLLx()<<endl;
+            }
+        }
+
+        for (int j=0; j<topoQUp[i]->down.size(); j++)
+        {
+            int newY = topoQUp[i]->down[j]->getLLy()\
+                        +topoQUp[i]->down[j]->getHeight();
+            if (newY>topoQUp[i]->getLLy())
+            {
+                topoQUp[i]->setLL(topoQUp[i]->getLLx(), newY);
+                // cout<<"  y refreshed to: "<<newY<<endl;
+            }
+            else
+            {
+                // cout<<"  y remained tobe: "<<topoQUp[i]->getLLy()<<endl;
+            }
+        }
+    }
+    cout<<">>>coord generated"<<endl;
+
+
+    // ========extracting max dimensions========
+    cout<<">>>extracting max dimensions"<<endl;
     for (int i=0; i<blk_amt; i++)
     {
         if (blkLib[i].getLLx()+blkLib[i].getWidth() > max_w)
         {
             max_w = blkLib[i].getLLx()+blkLib[i].getWidth();
-            cout<<"new W: "<<max_w<<" from "<<i<<endl;
+            // cout<<"new W: "<<max_w<<" from "<<i<<endl;
         }
         if (blkLib[i].getLLy()+blkLib[i].getHeight() > max_h)
         {
             max_h = blkLib[i].getLLy()+blkLib[i].getHeight();
-            cout<<"new H: "<<max_h<<" from "<<i<<endl;
+            // cout<<"new H: "<<max_h<<" from "<<i<<endl;
         }
     }
+}
+
+void DFSvisit(vector<Block> &blkLib, Block &targetBlk, int &timeStamp, vector<Block*> &Q, bool directUp) // directUp= 1:up  0:right
+{
+    timeStamp++;
+    if (directUp)
+    {
+        targetBlk.dUp = timeStamp;
+        targetBlk.DFSstateUp = 0;
+        for (int i=0; i<targetBlk.up.size(); i++)
+        {
+            if (targetBlk.up[i]->DFSstateUp==-1)
+            {
+                targetBlk.up[i]->DFSpredUp=&targetBlk;
+                DFSvisit(blkLib, *targetBlk.up[i], timeStamp, Q, directUp);
+            }
+        }
+        targetBlk.DFSstateUp = 1;
+        timeStamp++;
+        targetBlk.fUp = timeStamp;
+        Q.insert(Q.begin(), &targetBlk);
+    }
+    else
+    {
+        targetBlk.dRt = timeStamp;
+        targetBlk.DFSstateRt = 0;
+        for (int i=0; i<targetBlk.right.size(); i++)
+        {
+            if (targetBlk.right[i]->DFSstateRt==-1)
+            {
+                targetBlk.right[i]->DFSpredRt=&targetBlk;
+                DFSvisit(blkLib, *targetBlk.right[i], timeStamp, Q, directUp);
+            }
+        }
+        targetBlk.DFSstateRt = 1;
+        timeStamp++;
+        targetBlk.fRt = timeStamp;
+        Q.insert(Q.begin(), &targetBlk);
+    }
+    
 }
